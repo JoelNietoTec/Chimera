e077bd6899561440b8bd48797c291521ef059119
JoelNietoTec <joelnietotec@outlook.com>
2015-11-12 22:10:11 -0500

    New Position

--- client/templates/positions/new.js
+++ client/templates/positions/new.js
@@ -1,5 +1,12 @@
 Template.newPosition.onRendered(function() {
 	this.$(".dropdown").dropdown();
+	$('.ui.form').form({
+		fields: {
+			department: 'empty',
+			code: 'empty',
+			name: 'empty'
+		}
+	})
 });
 
 Template.newPosition.helpers({
@@ -11,5 +18,15 @@ Template.newPosition.helpers({
 Template.newPosition.events({
 	'submit form': function (e) {
 		e.preventDefault();
+		var position = {
+			department: $(e.target).find('[name=department]').val(),
+			code: $(e.target).find('[name=code]').val().toUpperCase(),
+			name: $(e.target).find('[name=name]').val()
+		};
+
+		Positions.insert(position);
+		$('.form')[0].reset();
+
+		Router.go('listPositions');
 	}
 });
