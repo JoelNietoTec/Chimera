91178f1ff1456994b8831f553306a0ec821683ac
JoelNietoTec <joelnietotec@outlook.com>
2015-11-19 10:04:29 -0500

    prof

--- client/templates/app/header.js
+++ client/templates/app/header.js
@@ -1,3 +1,11 @@
 Template.appHeader.onRendered(function(){
 	this.$('.ui.dropdown').dropdown();
 });
+
+Template.appHeader.events({
+	'click #logout': function (event) {
+		event.preventDefault();
+		if (Meteor.userId())
+			AccountsTemplates.logout();
+	}
+});
