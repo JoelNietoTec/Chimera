4ae41a35a0393bb3750e149def80724a86d8522f
Joel Nieto <jonieto@galindo.gala>
2015-12-09 18:27:24 -0500

    Search 1

--- client/templates/clients/list.js
+++ client/templates/clients/list.js
@@ -1,5 +1,14 @@
 Template.listClients.helpers({
 	clients: function () {
-		return Clients.find();
+		var search = Session.get('search');
+		return Clients.find({"businessName": /search/});
+	}
+});
+
+Template.listClients.events({
+	'input #search': function (event, template) {
+		Session.set('search', event.currentTarget.value);
+		var search = Session.get('search');
+		console.log(search);
 	}
 });
